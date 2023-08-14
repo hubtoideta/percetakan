7cf77047d327a6de1fa93b18a76f80091a14c6d7
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2023-08-14 11:53:36 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,24 +11,32 @@ class HomeController extends Controller
 {
     /* VIEW HOME */
     public function index(){
-        $userData = Auth::user();
+        /* User Login */
+        $userData = Auth::user(); 
+        /* Data profil user Login */
         $profileUser = ProfileUser::select('photo_profile')->find($userData->username);
-        $totalData = $profileUser->count();
+        /* Count data user */
+        $totalData = $profileUser ? $profileUser->count() : 0;
         
+        /* Photo profile account */
         if($totalData > 0){
             $fotoProfil = $profileUser->photo_profile;
         }else{
             $fotoProfil = 'none';
         }
 
+        /* Alert untuk Owner & Freelance */
         $alertData = false;
         if($userData->category == "Owner"){
             $informationStore = InformationStore::where("username_owner", $userData->username)->get();
             if($informationStore->count() == 0){
                 $alertData = true;
             }
+        }elseif($userData->category == "Freelance"){
+            $alertData = true;
         }
 
+        /* Return view */
         return view('dashView.Home', [
             'userLogin' => $userData,
             'alertData' => $alertData,
