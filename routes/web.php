fbf40978339ed82be2e508ed0008ebdc1a3ef4ba
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2023-09-12 00:52:37 +0800

    chore(grouping) membuat grup route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\ProfileUserController;
 use App\Http\Controllers\InformationStoreController;
 use App\Http\Controllers\pembayaranController;
 use App\Http\Controllers\PembelianPaketController;
+use App\Models\InformationStore;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,82 +23,112 @@ use App\Http\Controllers\PembelianPaketController;
 
 /* Route user non login */
 Route::group(['middleware' => 'guest'], function(){
-    /* ============== LOGIN ============== */
-        /* Show page login */
-        Route::get('/', [AuthController::class, 'loginViewPage'])->name('login');
-        /* Login proses */
-        Route::post('/', [AuthController::class, 'loginPost'])->name('login');
-    /* ============== LOGIN ============== */
+    Route::controller(AuthController::class)->group(function () {
+        /* ============== BEGIN LOGIN ============== */
+            /* Show page login */
+            Route::get('/','loginViewPage')->name('login');
+            /* Login proses */
+            Route::post('/','loginPost')->name('login');
+        /* ============== END LOGIN ============== */
 
-    /* ============== REGIS ============== */
-        /* Show page registrasi */
-        Route::get('/registrasi', [AuthController::class, 'registrasiViewPage'])->name('registrasi');
-        /* Registrasi Proses */
-        Route::post('/registrasi', [AuthController::class, 'registrasiPost'])->name('registrasi');
-    /* ============== REGIS ============== */
+        /* ============== BEGIN REGIS ============== */
+            /* Show page registrasi */
+            Route::get('/registrasi','registrasiViewPage')->name('registrasi');
+            /* Registrasi Proses */
+            Route::post('/registrasi','registrasiPost')->name('registrasi');
+        /* ============== END REGIS ============== */
 
-    /* ============== RESET PASSWORD ============== */
-        /* Show page lupa password */
-        Route::get('/lupa-password', [AuthController::class, 'lupaPasswordViewPage'])->name('lupa_password');
-        /* Lupa password & send token to email proses */
-        Route::post('/lupa-password', [AuthController::class, 'lupaPasswordPost'])->name('lupa_password');
-        /* Show page ubah passwrod with token url */
-        Route::get('/reset-password/{token}', [AuthController::class, 'passwordResetViewPage'])->name('password.reset');
-        /* Ubah Password Proses */
-        Route::post('/update-password', [AuthController::class, 'passwordResetPost'])->name('password.update');
-    /* ============== RESET PASSWORD ============== */
+        /* ============== BEGIN RESET PASSWORD ============== */
+            /* Show page lupa password */
+            Route::get('/lupa-password','lupaPasswordViewPage')->name('lupa_password');
+            /* Lupa password & send token to email proses */
+            Route::post('/lupa-password','lupaPasswordPost')->name('lupa_password');
+            /* Show page ubah passwrod with token url */
+            Route::get('/reset-password/{token}','passwordResetViewPage')->name('password.reset');
+            /* Ubah Password Proses */
+            Route::post('/update-password','passwordResetPost')->name('password.update');
+        /* ============== END RESET PASSWORD ============== */
+    });
 });
 
 /* Route user Login */
 Route::group(['middleware' => 'auth'], function(){
-    /* Show page home */
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-    /* Logut proses */
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    /* ============== PROFILE & ACCOUNT ============== */
-        /* Show page profil dan akun */
-        Route::get('/lihat-profile', [ProfileUserController::class, 'index'])->name('profile');
-        /* input data profil proses */
-        Route::post('/input-profile', [ProfileUserController::class, 'create'])->name('inputProfile');
-        /* update data profil proses */
-        Route::post('/update-profile', [ProfileUserController::class, 'update'])->name('editProfile');
-        /* update data email proses */
-        Route::post('/update-email', [ProfileUserController::class, 'updateEmail'])->name('editEmail');
-        /* update data password proses */
-        Route::post('/update-password', [ProfileUserController::class, 'updatePassword'])->name('editPassword');
-    /* ============== PROFILE & ACCOUNT ============== */
     
-    /* ============== SETTINGS PRICE PAKET DEVELOPER PANEL ============== */
-        /* Show page kelolah Paket */
-        Route::get('/paket-langganan', [DataPaketController::class, 'index'])->name('paket');
-        /* EDIT HARGA PAKET */
-        Route::post('/edit-harga-paket', [DataPaketController::class, 'updateHargaPaket'])->name('editPaket');
-        /* EDIT HARGA DISKON DURASI PAKET */
-        Route::post('/edit-diskon-paket', [DataPaketController::class, 'updateDiskonPaket'])->name('editDiskon');
-    /* ============== SETTINGS PRICE PAKET DEVELOPER PANEL ============== */
-        
-        
-        Route::get('/pembelian-paket', [PembelianPaketController::class, 'index'])->name('pembelianPaket');
-        Route::post('/pembelian-paket', [PembelianPaketController::class, 'findData'])->name('cari');
+    /* ----------- ALL USER PANEL ----------- */
+        /* Show page home */
+        Route::get('/home', [HomeController::class, 'index'])->name('home');
+        /* Logut proses */
+        Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-        Route::get('/data-toko', [dataStoreController::class, 'index'])->name('dataToko');
-        Route::post('/data-toko', [dataStoreController::class, 'findData'])->name('cari');
+        /* ============== BEGIN PROFILE & ACCOUNT ALL USER PANEL ============== */
+            Route::controller(ProfileUserController::class)->group(function () {
+                /* Show page profil dan akun */
+                Route::get('/lihat-profile','index')->name('profile');
+                /* input data profil proses */
+                Route::post('/input-profile','create')->name('inputProfile');
+                /* update data profil proses */
+                Route::post('/update-profile','update')->name('editProfile');
+                /* update data email proses */
+                Route::post('/update-email','updateEmail')->name('editEmail');
+                /* update data password proses */
+                Route::post('/update-password','updatePassword')->name('editPassword');
 
-    /* ============== PEMBELIAN PAKET OWNER PANEL ============== */
-        Route::get('/pembayaran-premium', [pembayaranController::class, 'premiumPaket'])->name('checkoutPremium');
-        Route::get('/pembayaran-business', [pembayaranController::class, 'businessPaket'])->name('checkoutBusiness');
-        Route::post('/pembayaran', [pembayaranController::class, 'checkoutPost'])->name('checkoutPost');
-    /* ============== PEMBELIAN PAKET OWNER PANEL ============== */
-    
+            });
+        /* ============== END PROFILE & ACCOUNT ALL USER PANEL ============== */
+    /* ----------- ALL USER PANEL ----------- */
 
-    /* ============== DATA STORE OWNER PANEL ============== */
-        /* Show page kelolah toko */
-        Route::get('/kelolah-toko', [InformationStoreController::class, 'index'])->name('kelolah-toko');
-        /* Input data toko proses */
-        Route::post('/input-toko', [InformationStoreController::class, 'create'])->name('input-toko');
-        /* Edit data toko proses */
-        Route::post('/edit-toko', [InformationStoreController::class, 'update'])->name('edit-toko');
-    /* ============== DATA STORE OWNER PANEL ============== */
-    
+
+    /* ----------- USER DEVELOPER PANEL ----------- */
+
+        /* ============== BEGIN SETTINGS PRICE PAKET USER DEVELOPER PANEL ============== */
+            Route::controller(DataPaketController::class)->group(function () {
+                /* Show page kelolah Paket */
+                Route::get('/paket-langganan','index')->name('paket');
+                /* EDIT HARGA PAKET */
+                Route::post('/edit-harga-paket','updateHargaPaket')->name('editPaket');
+                /* EDIT HARGA DISKON DURASI PAKET */
+                Route::post('/edit-diskon-paket','updateDiskonPaket')->name('editDiskon');
+            });
+        /* ============== END SETTINGS PRICE PAKET USER DEVELOPER PANEL ============== */
+
+        /* ============== BEGIN DATA PEMBELIAN PAKET USER DEVELOPER PANEL ============== */
+            Route::controller(PembelianPaketController::class)->group(function () {
+                Route::get('/pembelian-paket','index')->name('pembelianPaket');
+                Route::post('/pembelian-paket','findData')->name('cari');
+            });
+        /* ============== END DATA PEMBELIAN PAKET USER DEVELOPER PANEL ============== */
+
+        /* ============== BEGIN DATA TOKO USER DEVELOPER PANEL ============== */
+            Route::controller(dataStoreController::class)->group(function () {
+                Route::get('/data-toko','index')->name('dataToko');
+                Route::post('/data-toko','findData')->name('cari');
+            });
+        /* ============== END DATA TOKO USER DEVELOPER PANEL ============== */
+
+    /* ----------- USER DEVELOPER PANEL ----------- */
+
+
+    /* ----------- USER OWNER PANEL ----------- */
+
+        /* ============== BEGIN PEMBELIAN PAKET OWNER PANEL ============== */
+            Route::controller(pembayaranController::class)->group(function () {
+                Route::get('/pembayaran-premium','premiumPaket')->name('checkoutPremium');
+                Route::get('/pembayaran-business','businessPaket')->name('checkoutBusiness');
+                Route::post('/pembayaran','checkoutPost')->name('checkoutPost');
+            });
+        /* ============== END PEMBELIAN PAKET OWNER PANEL ============== */
+
+        /* ============== BEGIN DATA STORE OWNER PANEL ============== */
+            Route::controller(InformationStoreController::class)->group(function () {
+                /* Show page kelolah toko */
+                Route::get('/kelolah-toko','index')->name('kelolah-toko');
+                /* Input data toko proses */
+                Route::post('/input-toko','create')->name('input-toko');
+                /* Edit data toko proses */
+                Route::post('/edit-toko','update')->name('edit-toko');
+            });
+        /* ============== END DATA STORE OWNER PANEL ============== */
+
+    /* ----------- USER OWNER PANEL ----------- */
+
 });
